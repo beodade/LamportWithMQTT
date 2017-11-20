d40dac711609fb69a09d2f7d0f22629ff835aa90
Ihadadene <ihadadene@Ihadadenes-MacBook-Pro.local>
2017-11-20 17:44:43 +0100

    publisher send request with site id and time

--- publisher.py
+++ publisher.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
 import pika
 import sys
-class Publisher(Object):
+class Publisher(object):
 	def __init__(self,exchange_name,queue_name):
 		self.exchange_name=exchange_name
 		self.queue_name=queue_name
 		connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 		self.connection=connection
-		channel = connection.channel()
-		channel.exchange_declare(exchange=exchange_name,exchange_type='fanout')
-		self.site_number=int(exchange_name[:1])
+		self.channel = connection.channel()
+		self.channel.exchange_declare(exchange=exchange_name,exchange_type='fanout')
+		#self.site_number=int(exchange_name[:1])
+		self.site_number="1"
 	def send_request(self,time):
-		message = "{"+self.site_number+"}"+"{"+time+"}"
+		message = "{"+self.site_number+"}"+"{"+str(time)+"}"
 		self.channel.basic_publish(exchange=self.exchange_name,routing_key='',body=message)
 	def close_connection(self):
 		self.connection.close()
 
-if __name__= main :
-	if len(sys.argv) != 3
+if __name__== "__main__" :
+	if len(sys.argv) != 3:
 		print("Warning : python publisher.py exchange_name queue_name")
 	else:
 		pub=Publisher(sys.argv[1],sys.argv[2])
